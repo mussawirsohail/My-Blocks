2406c9cfc0135df2ffc31c5d59b9a4b24b4777f6
Mussawir Sohail <159765711+mussawirsohail@users.noreply.github.com>
2024-10-11 12:23:20 +0500

    Add files via upload

---
+++ src/app/responsive/page.tsx
@@ -0,0 +1,104 @@
+// import Link from "next/link";
+// import Header from "../components/header";
+// export default function Responsive() {
+//     return (
+//       <body>
+//         <Header/>
+//       <div className="parentContainer">
+//         <div className="childContainer bounce-in-right">
+//           <div className="imageContainer">
+//           <img src="Typescript.png"alt="Typescript"className="pic"/>
+//           </div>
+//           <h1 className="title">Typescript</h1>
+//           <p className="description">
+//           TypeScript is a typed superset of JavaScript. 
+//         {" "}
+//           </p>
+//           <Link href={"/ts"}>
+//           <button className="readMore">Read More</button>
+//           </Link>
+//         </div>
+  
+//         <div className="childContainer bounce-in-right">
+//           <div className="imageContainer">
+//             <img src="css.png"alt="CSS"className="pic"/>
+//           </div>
+//           <h1 className="title">CSS</h1>
+//           <p className="description">CSS styles HTML elements to control layout, design, and presentation.
+//         {" "}
+//           </p>
+//           <button className="readMore">Read More</button>
+//         </div>
+  
+//         <div className="childContainer bounce-in-right">
+//           <div className="imageContainer">
+//             <img src="NEXTJS.PNG"alt="JS"className="pic"/>
+//           </div>
+//           <h1 className="title">Next JS</h1>
+//           <p className="description">
+//           Next.js is a React framework for building fast and server-rendered.
+//         {" "}
+//           </p>
+//           <button className="readMore">Read More</button>
+//         </div>
+//         <div className="childContainer bounce-in-right">
+//           <div className="imageContainer">
+//             <img src="JavaScript.png"alt="java"className="pic"/>
+//           </div>
+//           <h1 className="title">Java Script</h1>
+//           <p className="description">
+//           JavaScript is a versatile programming language  
+//         {" "}
+//           </p>
+//           <Link href={"/js"}>
+//           <button className="readMore">Read More</button>
+//           </Link>        
+//         </div>
+//         <div className="childContainer bounce-in-right">
+//           <div className="imageContainer">
+//             <img src="phyton.jpg"alt="phy"className="pic"/>
+//           </div>
+//           <h1 className="title">Phyton</h1>
+//           <p className="description">
+//           Python is a high-level, versatile programming language
+//         {" "}
+//           </p>
+//           <button className="readMore">Read More</button>
+//         </div>
+//         <div className="childContainer bounce-in-right">
+//           <div className="imageContainer">
+//             <img src="csharp.png"alt="c#"className="pic"/>
+//           </div>
+//           <h1 className="title">C Sharp</h1>
+//           <p className="description">
+//           C# is a modern, object-oriented programming language 
+//         {" "}
+//           </p>
+//           <button className="readMore">Read More</button>
+//         </div>
+//         <div className="childContainer bounce-in-right">
+//           <div className="imageContainer">
+//             <img src="word.jpg"alt="word"className="pic"/>
+//           </div>
+//           <h1 className="title">MS Word</h1>
+//           <p className="description">
+//           Microsoft Word is a word processing software used for creating, editing, and formatting text documents.
+//         {" "}
+//           </p>
+//           <button className="readMore">Read More</button>
+//         </div>
+//         <div className="childContainer bounce-in-right">
+//           <div className="imageContainer">
+//             <img src="excel.jpg"alt="excel"className="pic"/>
+//           </div>
+//           <h1 className="title">MS Excel</h1>
+//           <p className="description">
+//           Microsoft Excel is a spreadsheet application used for data organization, analysis, and visualization through formulas, functions, and charts.
+//         {" "}
+//           </p>
+//           <button className="readMore">Read More</button>
+//         </div>
+//       </div>
+//       </body>
+//     );
+//   }
